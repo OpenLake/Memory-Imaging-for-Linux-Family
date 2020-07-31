44cc326dc3dce827a4ddd1c378d99f9be8d907b0
Ishaan Shrivastava <ishaanshrivastava1998ad@gmail.com>
2020-08-01 01:41:01 +0530

    fixed some bugs in int search and other searches as a whole

--- main.cpp
+++ main.cpp
@@ -6,26 +6,14 @@ using namespace std;
 int main()
 {
 	long id,BA;
+	int a =1234;
 	cout << "Enter PID of the Process\n";
-	cin >> id;
-	ProcessManager P(id);
-	P.span = 12;
+	cin>>id;
+
+	ProcessManager P(id,0,4);
+	vector <long> v= P.scanProcess(1234);
+	/*ProcessManager P(id);
 	P.FindBaseAddress("[stack]");
-	P.PrintContent();
-	// cout << "Enter BaseAddress\n";
-	// cin >> BA;
-	// ProcessManager P(id,BA,16);
-
-	// P.getContent("int");
-
-	char test[16] = "7f4644c29001";
-	// int **add;
-	// P.getAddresses();
-	// // int size ;
-	// // // size = P.AddressesRows;
-	// // cout << "span:" << P.span << endl;
-	// // cout << "Addresses size: "<< P.AddressesRows <<endl;
-	// P.identifierFunction(test);
-	// cout <<  P.Addresses  << endl << add <<endl;
+	P.PrintContent();*/
 	return 0;
 }
